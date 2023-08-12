d9282349ffeb39f07320ad98c40c39f38f24dd58
Daniel Burgess <dan-burgess@hotmail.com>
2023-08-12 12:53:17 +0930

    Initial ui layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
+const Form = () => {
+  return (
+    <div className="new-user-form">
+      <form>
+        <div>
+          <p>
+            <label>Username</label>
+            <input />
+          </p>
+          <p>
+            <label>Age (years)</label>
+            <input />
+          </p>
+          <button>Add User</button>
+        </div>
+      </form>
+    </div>
+  )
+}
 
 function App() {
   return (
     <div>
-      
+      <Form />
     </div>
   );
 }
